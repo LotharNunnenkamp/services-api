5d49f68965281d6bb7a2f329b999db44bca15333
Lothar <lothar223@hotmail.com>
2024-11-26 09:29:20 -0300

    feat: project started

---
+++ src/main/java/br/com/ada/t1172/services_api/ServicesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.t1172.services_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ada/t1172/services_api/ServicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.t1172.services_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
